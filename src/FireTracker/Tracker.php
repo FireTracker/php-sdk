a5cbdeb821ad5cc7205d4ae4bc5a616ddb73631f
domino <elwadiamine@gmail.com>
2016-07-12 17:46:04 +0100

    first commit

--- src/FireTracker/Tracker.php
+++ src/FireTracker/Tracker.php
@@ -1,5 +1,7 @@
 <?php
 namespace FireTracker;
+use Unirest\Request;
+
 /**
  *
  */
@@ -19,16 +21,118 @@ class Tracker
   public static $_FIRE_ENV_STAGING = 'STAGING';
   public static $_FIRE_ENV_PRODUCTION = 'PRODUCTION';
 
+  public static $_FIRE_ENV_VAR_NAME="_FIRE_ENV";
+  public static $_FIRE_ENDPOINT="https://target.firetracker.io";
+
+  public static $_FIRE_USER_SECRET="_FIRE_USER_SECRET";
+  public static $_FIRE_USER_KEY="_FIRE_USER_KEY";
+
   public static function test(){
       echo "Tracker Fired";
   }
-  public static function Fire(Exception $_x_,$level=null,$env=null){
-    if(!defined(FT_API_KEY)||!defined(FT_APP_KEY)){
+
+  /**
+   * @param $level
+   * @param string $message
+   * @param string $context
+   * @return mixed
+   */
+  public static function Fire($level,$message="",$context=""){
+    $headers = array('Accept' => 'application/json');
+    //api auth infos
+    $userSecret=(isset($GLOBALS[self::$_FIRE_USER_SECRET]))?$GLOBALS[self::$_FIRE_USER_SECRET]:null;
+    $userKey=(isset($GLOBALS[self::$_FIRE_USER_KEY]))?$GLOBALS[self::$_FIRE_USER_KEY]:null;
+    //check auth
+    if($userKey==null || $userSecret==null){
       return false;
     }
-    echo "Fire Exception".self::$_FIRE_LEVEL_DEBUG;
-  }
-  /*public static function Fire($code="",$message="",$file="",$line="",$level=null,$env=null){
+    //default env value DEVELOPMENT
+    $env=self::$_FIRE_ENV_DEVELOPMENT;
+    //array to check sended value
+    $targetEnv=array(
+      self::$_FIRE_ENV_DEVELOPMENT,
+      self::$_FIRE_ENV_TEST,
+      self::$_FIRE_ENV_PRODUCTION,
+      self::$_FIRE_ENV_STAGING,
+    );
 
-  }*/
+    if(isset($GLOBALS[self::$_FIRE_ENV_VAR_NAME])){
+      if(in_array($GLOBALS[self::$_FIRE_ENV_VAR_NAME],$targetEnv)){
+        $env=$GLOBALS[self::$_FIRE_ENV_VAR_NAME];
+      }
+    }
+    $query = array(
+        'fire_level' => $level,
+        'fire_message' => $message,
+        'fire_context' => $context,
+        'fire_env' => $env,
+        'fire_key'=>$userKey,
+        'fire_secret'=>$userSecret,
+    );
+    Request::post(self::$_FIRE_ENDPOINT,$headers,$query);
+  }
+
+  /**
+   * @param string $message
+   * @param string $context
+   */
+  public static function FireEmergency($message="",$context=""){
+      self::Fire(self::$_FIRE_LEVEL_EMERGENCY,$message,$context);
+  }
+
+  /**
+   * @param string $message
+   * @param string $context
+   */
+  public static function FireDebug($message="",$context=""){
+    self::Fire(self::$_FIRE_LEVEL_DEBUG,$message,$context);
+  }
+
+  /**
+   * @param string $message
+   * @param string $context
+   */
+  public static function FireInfo($message="",$context=""){
+    self::Fire(self::$_FIRE_LEVEL_INFO,$message,$context);
+  }
+
+  /**
+   * @param string $message
+   * @param string $context
+   */
+  public static function FireNotice($message="",$context=""){
+    self::Fire(self::$_FIRE_LEVEL_NOTICE,$message,$context);
+  }
+
+  /**
+   * @param string $message
+   * @param string $context
+   */
+  public static function FireWarning($message="",$context=""){
+    self::Fire(self::$_FIRE_LEVEL_WARNING,$message,$context);
+  }
+
+  /**
+   * @param string $message
+   * @param string $context
+   */
+  public static function FireError($message="",$context=""){
+    self::Fire(self::$_FIRE_LEVEL_ERROR,$message,$context);
+  }
+
+  /**
+   * @param string $message
+   * @param string $context
+   */
+  public static function FireAlert($message="",$context=""){
+    self::Fire(self::$_FIRE_LEVEL_ALERT,$message,$context);
+  }
+
+  /**
+   * @param string $message
+   * @param string $context
+   */
+  public static function FireCritical($message="",$context=""){
+    self::Fire(self::$_FIRE_LEVEL_CRITICAL,$message,$context);
+  }
 }
